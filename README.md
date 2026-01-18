7044cb3a267b846bf7e72bc50bf326129409543f
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2026-01-19 02:46:23 +0300

    [core] Implemented CRDL

---
+++ README.md
@@ -0,0 +1,9 @@
+# REST API: User Data Aggregation
+
+Start a Service
+```bash
+docker compose up -d
+
+go mod tidy
+go build -o main . && ./main
+```
--- main.go
+++ main.go
@@ -1,34 +1,366 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"database/sql"
+	"encoding/json"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// XXX (ames0k0): Application Struct ?
+type application struct {
+	logger *slog.Logger
+	dbpool *pgxpool.Pool
+}
+
+type UserSubscriptions struct {
+	Id pgtype.UUID `json:"id"`
+	User_id pgtype.UUID `json:"user_id"`
+	Service_name string `json:"service_name"`
+	Price int `json:"price"`
+	Start_date string `json:"start_date"`
+	End_date *string `json:"end_date"`
+}
+
 // TODO (ames0k0): sId :: string -> UUID
+func main() {
+	// LOGGING init
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-func subscriptionsCreateHandler(w http.ResponseWriter, r *http.Request, sId string) {
-	fmt.Fprintf(w, "Create, I love %s!", r.URL.Path[1:])
+	// DATABASE connection
+	// dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	dbpool, err := pgxpool.New(context.Background(), "postgresql://postgres:simple@localhost:5454/templates__postgres")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "Connected to the database\n")
+	defer dbpool.Close()
+
+	app := &application{logger: logger, dbpool: dbpool}
+
+	http.HandleFunc("/subscriptions/", app.subscriptionsHandler)
+	http.HandleFunc("/subscriptions/list", app.subscriptionsListHandler)
+	http.HandleFunc("/subscriptions/calc-total-costs/", app.subscriptionsCalcTotalCostsHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil && err != http.ErrServerClosed {
+		app.logger.Error(
+			"Listen and Server FAILED",
+			"err",
+			err.Error(),
+		)
+	}
 }
 
-func subscriptionsReadHandler(w http.ResponseWriter, r *http.Request, sId string) {
-	fmt.Fprintf(w, "Read, I love %s!", r.URL.Path[1:])
+func (app *application) subscriptionsHandler(w http.ResponseWriter, r *http.Request) {
+	var user_subscription UserSubscriptions
+
+	// TODO (ames0k0): PG.Get
+	if (r.Method != http.MethodPost) {
+		requiredQP := []string{"id"}
+		optionalQP := []string{}
+		rRQP, _, err := queryParamsLoader(w, r, requiredQP, optionalQP)
+
+		if err != nil {
+			return
+		}
+
+		query := `
+		SELECT * FROM user_subscriptions
+		WHERE id = $1
+		`
+
+		err = app.dbpool.QueryRow(
+			context.Background(),
+			query,
+			rRQP["id"],
+		).Scan(
+			&user_subscription.Id,
+			&user_subscription.User_id,
+			&user_subscription.Service_name,
+			&user_subscription.Price,
+			&user_subscription.Start_date,
+			&user_subscription.End_date,
+		)
+
+		if err != nil {
+			http.Error(
+				w,
+				"UserSubscriptions is not found!",
+				http.StatusNotFound,
+			)
+			return
+		}
+	}
+
+	switch r.Method {
+	case http.MethodPost:
+		app.subscriptionsCreateHandler(w, r)
+	case http.MethodGet:
+		app.subscriptionsReadHandler(w, r, user_subscription)
+	case http.MethodPatch:
+		app.subscriptionsUpdateHandler(w, r, user_subscription)
+	case http.MethodDelete:
+		app.subscriptionsDeleteHandler(w, r, user_subscription)
+	}
 }
 
-func subscriptionsUpdateHandler(w http.ResponseWriter, r *http.Request, sId string) {
+func queryParamsLoader(w http.ResponseWriter, r *http.Request, requiredQP []string, optionalQP[]string) (map[string]string, map[string]string, error) {
+	rRQP := make(map[string]string)
+	rOQP := make(map[string]string)
+	var rMRQP []string
+
+	queryParams := r.URL.Query()
+
+	for _, key := range requiredQP {
+		value := queryParams.Get(key)
+		if value == "" {
+			rMRQP = append(rMRQP, key)
+		} else {
+			rRQP[key] = value
+		}
+	}
+
+	for _, key := range optionalQP {
+		value := queryParams.Get(key)
+		rOQP[key] = value
+	}
+
+	if len(rMRQP) > 0 {
+		http.Error(w, "Missing required params: " + strings.Join(rMRQP, ","), http.StatusBadRequest)
+		return rRQP, rOQP, errors.New("Missing required params")
+	}
+
+	return rRQP, rOQP, nil
+}
+
+func (app *application) subscriptionsCreateHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(
+			w,
+			"Could not parse request.Form",
+			http.StatusBadRequest,
+		)
+		app.logger.Error(
+			"Could not parse request.Form",
+			"err",
+			err.Error(),
+			"request.Form",
+			r.Form,
+		)
+		return
+	}
+
+	requiredFD := []string{"user_id", "service_name", "price", "start_date"}
+	var rMRFD[]string
+
+	for _, key := range requiredFD {
+		value := r.FormValue(key)
+		if value == "" {
+			rMRFD = append(rMRFD, key)
+		}
+	}
+
+	if len(rMRFD) > 0 {
+		http.Error(
+			w,
+			"Missing required form data: " + strings.Join(rMRFD, ", "),
+			http.StatusBadRequest,
+		)
+		app.logger.Error(
+			"Missing required form data: " + strings.Join(rMRFD, ", "),
+			"request.form",
+			r.Form,
+		)
+		return
+	}
+
+	var end_date sql.NullString
+
+	if r.FormValue("end_date") == "" {
+		end_date = sql.NullString{String: r.FormValue("end_date"), Valid: false}
+	} else {
+		end_date = sql.NullString{String: r.FormValue("end_date"), Valid: true}
+	}
+
+	query := `
+	INSERT INTO user_subscriptions (user_id, service_name, price, start_date, end_date)
+	VALUES ($1, $2, $3, $4, $5)
+	`
+
+	_, err = app.dbpool.Exec(
+		context.Background(),
+		query,
+		r.FormValue("user_id"),
+		r.FormValue("service_name"),
+		r.FormValue("price"),
+		r.FormValue("start_date"),
+		end_date,
+	)
+
+	if err != nil {
+		http.Error(
+			w,
+			"Could not dbpool.Exec(Insert)",
+			http.StatusInternalServerError,
+		)
+		app.logger.Error(
+			"Could not dbpool.Exec(Insert)",
+			"err",
+			err.Error(),
+			"request.Form",
+			r.Form,
+		)
+		return
+	}
+}
+
+func (app *application) subscriptionsReadHandler(w http.ResponseWriter, _ *http.Request, user_subscription UserSubscriptions) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err := json.NewEncoder(w).Encode(user_subscription)
+	if err != nil {
+		http.Error(
+			w,
+			"Could not json.encode(UserSubscriptions)",
+			http.StatusInternalServerError,
+		)
+		app.logger.Error(
+			"Could not json.encode(UserSubscriptions)",
+			"err",
+			err.Error(),
+			"user_subscription",
+			user_subscription,
+		)
+		return
+	}
+}
+
+func (app *application) subscriptionsUpdateHandler(w http.ResponseWriter, r *http.Request, user_subscription UserSubscriptions) {
+	requiredQP := []string{"subscription_id"}
+	optionalQP := []string{"user_id", "service_name", "price", "start_date", "end_date"}
+	rRQP, rOQP, err := queryParamsLoader(w, r, requiredQP, optionalQP)
+	_ = rRQP
+	_ = rOQP
+
+	if err != nil {
+		return
+	}
+
+	// TODO (ames0k0): PG.Update
 	fmt.Fprintf(w, "Update, I love %s!", r.URL.Path[1:])
 }
 
-func subscriptionsDeleteHandler(w http.ResponseWriter, r *http.Request, sId string) {
-	fmt.Fprintf(w, "Delete, I love %s!", r.URL.Path[1:])
+
+func (app *application) subscriptionsDeleteHandler(w http.ResponseWriter, _ *http.Request, user_subscription UserSubscriptions) {
+	query := `DELETE FROM user_subscriptions WHERE id = $1`
+
+	_, err := app.dbpool.Exec(
+		context.Background(),
+		query,
+		user_subscription.Id,
+	)
+
+	if err != nil {
+		http.Error(w, "Could not dbpool.Exec(Delete)", http.StatusInternalServerError)
+		app.logger.Error("Could not dbpool.Exec(Delete)", "err", err.Error())
+		return
+	}
 }
 
-func subscriptionsListHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "List, I love %s!", r.URL.Path[1:])
+func (app *application) subscriptionsListHandler(w http.ResponseWriter, r *http.Request) {
+	result_limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+	var users_subscriptions []UserSubscriptions
+
+	if err != nil || result_limit > 50 {
+		result_limit = 50
+	}
+
+	rows, err := app.dbpool.Query(
+		context.Background(),
+		"SELECT * FROM user_subscriptions LIMIT $1",
+		result_limit,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user_subscription UserSubscriptions
+		err := rows.Scan(
+			&user_subscription.Id,
+			&user_subscription.User_id,
+			&user_subscription.Service_name,
+			&user_subscription.Price,
+			&user_subscription.Start_date,
+			&user_subscription.End_date,
+		)
+		if err != nil {
+			http.Error(
+				w,
+				"Could not dbpool.Query(List.Load)",
+				http.StatusInternalServerError,
+			)
+			app.logger.Error(
+				"Could not dbpool.Query(List.Load)",
+				"err",
+				err.Error(),
+			)
+		}
+		users_subscriptions = append(users_subscriptions, user_subscription)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		http.Error(
+			w,
+			"Could not dbpool.Query(Rows)",
+			http.StatusInternalServerError,
+		)
+		app.logger.Error(
+			"Could not dbpool.Query(Rows)",
+			"err",
+			err.Error(),
+		)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(users_subscriptions)
+
+	if err != nil {
+		http.Error(
+			w,
+			"Could not json.encode([]UserSubscriptions)",
+			http.StatusInternalServerError,
+		)
+		app.logger.Error(
+			"Could not json.encode([]UserSubscriptions)",
+			"err",
+			err.Error(),
+			"users_subscriptions",
+			users_subscriptions,
+		)
+		return
+	}
 }
 
-func calcSubscriptionsTotalCostsHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) subscriptionsCalcTotalCostsHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 	userId := queryParams.Get("userId")
 	if userId == "" {
@@ -36,49 +368,4 @@ func calcSubscriptionsTotalCostsHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	fmt.Fprintf(w, "CalcTotalCosts, I love %s!", r.URL.Path[1:])
-}
-
-func subscriptionsHandler(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	// fmt.Println(queryParams)
-	subscriptionId := queryParams.Get("subscriptionId")
-	// fmt.Fprintf(w, "Read, I love %s!", r.URL.Path[1:])
-
-	switch r.Method {
-	case http.MethodPost:
-		subscriptionsCreateHandler(w, r, subscriptionId)
-	case http.MethodGet:
-		if subscriptionId != "" {
-			subscriptionsReadHandler(w, r, subscriptionId)
-		} else {
-			subscriptionsListHandler(w, r)
-		}
-	case http.MethodPatch:
-		// TODO (ames0k0): Better way to handle
-		if subscriptionId == "" {
-			http.Error(w, "Name parameter is required (`subscriptionId`)", http.StatusBadRequest)
-			return
-		}
-		subscriptionsUpdateHandler(w, r, subscriptionId)
-	case http.MethodDelete:
-		if subscriptionId == "" {
-			http.Error(w, "Name parameter is required (`subscriptionId`)", http.StatusBadRequest)
-			return
-		}
-		subscriptionsDeleteHandler(w, r, subscriptionId)
-	}
-}
-
-/**
-Maybe
-	/subscriptions/
-		{CRUD}
-	/subscriptions/list
-	/subscriptions/calc-total-costs ??
-*/
-
-func main() {
-	http.HandleFunc("/subscriptions/", subscriptionsHandler)
-	http.HandleFunc("/calc-total-costs/", calcSubscriptionsTotalCostsHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
